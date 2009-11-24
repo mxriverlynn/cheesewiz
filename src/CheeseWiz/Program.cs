bae56f154d72fb0bd8b429c12edd5ff4f36666cf
Derick Bailey <derickbailey@gmail.com>
2009-11-24 14:42:24 -0600

    added runtime implementation for parsing a real .inf file and resource files

--- src/CheeseWiz/Program.cs
+++ src/CheeseWiz/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using CheeseWiz.InfModel;
+using CheeseWiz.InfParsing;
+using CheeseWiz.InfRepairing;
 
 namespace CheeseWiz
 {
@@ -9,6 +10,43 @@ namespace CheeseWiz
 	{
 		static void Main(string[] args)
 		{
+			if (args == null || args.Length == 0)
+			{
+				Console.WriteLine("You must provide the location of a .inf file to repair.");
+				return;
+			}
+
+			string infFile = args[0];
+
+			string infContents = ReadInfFile(infFile);
+			Inf inf = ParseInfFile(infContents);
+			RepairInfFile(inf);
+			WriteInfFile(infFile, inf);
+		}
+
+		private static string ReadInfFile(string file)
+		{
+			return File.ReadAllText(file);
+		}
+
+		private static void WriteInfFile(string infFile, Inf inf)
+		{
+			string infContents = inf.RebuildInf();
+			File.WriteAllText(infFile, infContents);
+		}
+
+		private static void RepairInfFile(Inf inf)
+		{
+			IResourceFileProcessor processor = new ResourceFileProcessor();
+			InfRepairer repairer = new InfRepairer(processor);
+			repairer.Repair(inf);
+		}
+
+		private static Inf ParseInfFile(string infContents)
+		{
+			InfParser parser = new InfParser();
+			Inf inf = parser.Parse(infContents);
+			return inf;
 		}
 	}
 }
---
+++ src/CheeseWiz/ResourceFileProcessor.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.Text.RegularExpressions;
+using CheeseWiz.InfModel;
+using CheeseWiz.InfRepairing;
+
+namespace CheeseWiz
+{
+	internal class ResourceFileProcessor : IResourceFileProcessor
+	{
+		public SourceFile RenameFile(string folder, SourceFile file)
+		{
+			DirectoryInfo info = new DirectoryInfo(folder);
+			var sourceFile = Path.Combine(info.FullName, file.Filename);
+			var destinationFile = GetNewFileName(file.Filename, info.Name);
+			
+			File.Move(sourceFile, Path.Combine(info.FullName, destinationFile));
+
+			SourceFile renamedFile = new SourceFile(destinationFile, file.ReferenceNumber);
+			return renamedFile;
+		}
+
+		private string GetNewFileName(string originalFileName, string folderName)
+		{
+			var regex = new Regex(@"(?<begin>.*)(?<end>\.resources.dll)");
+			string newFileName = regex.Replace(originalFileName, "${begin}." + folderName + "${end}");
+			return newFileName;
+		}
+	}
+}
